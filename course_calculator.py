4024320c3e40cba0a26bbf86ce69b3f36e6d70ea
sofiadiaz <sdiaz@caltech.edu>
2024-11-07 13:17:31 -0800

    google analytics

--- course_calculator.py
+++ course_calculator.py
@@ -10,19 +10,23 @@ from io import BytesIO
 import time
 st.markdown(
     """
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-T91J78LJNN"></script>
+    <!-- Google Tag Manager -->
     <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-T91J78LJNN');
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-52GRQSL');
     </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52GRQSL"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     """,
     unsafe_allow_html=True
-
 )
-
 # Capture the lead_id from the URL if it exists
 query_params = st.experimental_get_query_params()
 lead_id = query_params.get('lead_id', [None])[0]  # Get the 'lead_id' parameter if present
