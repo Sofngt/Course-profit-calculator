f53be9d94a91ff2e6dabcf2731e2dd2c3ffd369e
sofiadiaz <sdiaz@caltech.edu>
2024-11-10 09:40:53 -0800

    fix

--- course_calculator.py
+++ course_calculator.py
@@ -10,15 +10,15 @@ from io import BytesIO
 import time
 st.markdown(
     """
-    <!-- Google Tag Manager (Head) -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-T91J78LJNN"></script>
     <script>
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-52GRQSL');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-T91J78LJNN');
     </script>
-    <!-- End Google Tag Manager -->
     """,
     unsafe_allow_html=True
 )
@@ -276,7 +276,7 @@ if st.button("Calculate"):
                     'monthlySales': '{course_sales[i]}',
                     'instructorPct': '{instructor_sales[i]*100}',
                     'organicPct': '{organic_sales[i]*100}',
-                    'affiliatePct': '{affiliate_sales[i]*100}'
+                    'affiliatePct': '{affiliate_sales[i]*100}',
                     'leadId': '{lead_id}'
                 }});
                 """ for i in range(num_courses)
