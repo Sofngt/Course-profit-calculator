a7c1ad3c65913fcd03d1523918032856cfcf03a6
sofiadiaz <sdiaz@caltech.edu>
2024-11-14 14:30:01 -0800

    tag change

--- course_calculator.py
+++ course_calculator.py
@@ -7,32 +7,22 @@ import base64
 from io import BytesIO
 import time
 
-# Corrected Google Tag Manager integration
+# Google Analytics 4 tag integration
 st.markdown(
     """
-    <!-- Google Tag Manager -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-T91J78LJNN"></script>
     <script>
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MLCWPL3X');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-T91J78LJNN');
     </script>
-    <!-- End Google Tag Manager -->
     """,
     unsafe_allow_html=True
 )
 
-# Corrected noscript tag for GTM
-st.markdown(
-    """
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLCWPL3X"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    """,
-    unsafe_allow_html=True
-)
 
 # Capture lead_id from URL query parameters using the updated Streamlit property
 query_params = st.query_params
